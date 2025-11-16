f3f9ee85a19be1b27eb89627a064050c434ab11f
trongtomo <binguyenquoctrong@gmail.com>
2025-11-16 23:11:20 +0700

    remove overlap icon header

--- src/components/Header.js
+++ src/components/Header.js
@@ -91,10 +91,7 @@ export default function Header() {
               <button 
                 onClick={handleMobileMenuToggle}
                 className="text-gray-700 hover:text-gray-900 focus:outline-none focus:text-gray-900"
-              >
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" suppressHydrationWarning>
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
+              > 
               </button>
             </div>
           </div>
